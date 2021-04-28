e95aabc3344cd7a15978b9b71e23b09e740d788a
leoafarias <leokcta@gmail.com>
2021-04-27 22:26:15 -0400

    Commit

--- lib/src/constants.dart
+++ lib/src/constants.dart
@@ -42,6 +42,7 @@ final _env = Platform.environment;
 Credentials? get pubCredentials {
   final credEnv = _env['PUB_CREDENTIALS'];
   // Get credentials from Env var if it exists
+  // print(credEnv)
   if (credEnv != null) {
     return Credentials.fromJson(credEnv);
   }
