355ecd33b715822022842a14561bf884596b1c77
Ashutosh Agarwal <ashutoshagarwal2014@gmail.com>
2022-11-01 23:09:18 +0530

    Update recursive_paging.dart

--- lib/src/helpers/recursive_paging.dart
+++ lib/src/helpers/recursive_paging.dart
@@ -3,13 +3,15 @@ import '../pub_api_client_base.dart';
 
 Future<List<PackageResult>> recursivePaging(
     PubClient client, SearchResults prevResults) async {
+  final List<PackageResult> lstPackages = [];
   final packages = prevResults.packages;
   final nextPage = prevResults.next;
+  lstPackages.addAll(packages);
   if (nextPage != null) {
     final results = await client.nextPage(nextPage);
     final nextResults = await recursivePaging(client, results);
-    packages.addAll(nextResults);
+    lstPackages.addAll(nextResults);
   }
 
-  return packages;
+  return lstPackages;
 }
