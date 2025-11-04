d07b66a4ea3fbd69922d756b7757d45173e7214f
Leo Farias <leoafarias@gmail.com>
2025-11-04 16:00:09 -0500

    Format generated mapper files
    Generated code files have been reformatted by build_runner to follow
    Dart code style guidelines. All changes are formatting only.

--- lib/src/models/latest_version_model.mapper.dart
+++ lib/src/models/latest_version_model.mapper.dart
@@ -1,5 +1,6 @@
 // coverage:ignore-file
 // GENERATED CODE - DO NOT MODIFY BY HAND
+// dart format off
 // ignore_for_file: type=lint
 // ignore_for_file: unused_element, unnecessary_cast, override_on_non_overriding_member
 // ignore_for_file: strict_raw_type, inference_failure_on_untyped_parameter
@@ -22,14 +23,20 @@ class LatestVersionMapper extends ClassMapperBase<LatestVersion> {
   final String id = 'LatestVersion';
 
   static bool _$needUpdate(LatestVersion v) => v.needUpdate;
-  static const Field<LatestVersion, bool> _f$needUpdate =
-      Field('needUpdate', _$needUpdate);
+  static const Field<LatestVersion, bool> _f$needUpdate = Field(
+    'needUpdate',
+    _$needUpdate,
+  );
   static String _$latestVersion(LatestVersion v) => v.latestVersion;
-  static const Field<LatestVersion, String> _f$latestVersion =
-      Field('latestVersion', _$latestVersion);
+  static const Field<LatestVersion, String> _f$latestVersion = Field(
+    'latestVersion',
+    _$latestVersion,
+  );
   static PubPackage _$packageInfo(LatestVersion v) => v.packageInfo;
-  static const Field<LatestVersion, PubPackage> _f$packageInfo =
-      Field('packageInfo', _$packageInfo);
+  static const Field<LatestVersion, PubPackage> _f$packageInfo = Field(
+    'packageInfo',
+    _$packageInfo,
+  );
 
   @override
   final MappableFields<LatestVersion> fields = const {
@@ -40,9 +47,10 @@ class LatestVersionMapper extends ClassMapperBase<LatestVersion> {
 
   static LatestVersion _instantiate(DecodingData data) {
     return LatestVersion(
-        needUpdate: data.dec(_f$needUpdate),
-        latestVersion: data.dec(_f$latestVersion),
-        packageInfo: data.dec(_f$packageInfo));
+      needUpdate: data.dec(_f$needUpdate),
+      latestVersion: data.dec(_f$latestVersion),
+      packageInfo: data.dec(_f$packageInfo),
+    );
   }
 
   @override
@@ -59,41 +67,50 @@ class LatestVersionMapper extends ClassMapperBase<LatestVersion> {
 
 mixin LatestVersionMappable {
   String toJson() {
-    return LatestVersionMapper.ensureInitialized()
-        .encodeJson<LatestVersion>(this as LatestVersion);
+    return LatestVersionMapper.ensureInitialized().encodeJson<LatestVersion>(
+      this as LatestVersion,
+    );
   }
 
   Map<String, dynamic> toMap() {
-    return LatestVersionMapper.ensureInitialized()
-        .encodeMap<LatestVersion>(this as LatestVersion);
+    return LatestVersionMapper.ensureInitialized().encodeMap<LatestVersion>(
+      this as LatestVersion,
+    );
   }
 
   LatestVersionCopyWith<LatestVersion, LatestVersion, LatestVersion>
-      get copyWith => _LatestVersionCopyWithImpl(
-          this as LatestVersion, $identity, $identity);
+  get copyWith => _LatestVersionCopyWithImpl<LatestVersion, LatestVersion>(
+    this as LatestVersion,
+    $identity,
+    $identity,
+  );
   @override
   String toString() {
-    return LatestVersionMapper.ensureInitialized()
-        .stringifyValue(this as LatestVersion);
+    return LatestVersionMapper.ensureInitialized().stringifyValue(
+      this as LatestVersion,
+    );
   }
 
   @override
   bool operator ==(Object other) {
-    return LatestVersionMapper.ensureInitialized()
-        .equalsValue(this as LatestVersion, other);
+    return LatestVersionMapper.ensureInitialized().equalsValue(
+      this as LatestVersion,
+      other,
+    );
   }
 
   @override
   int get hashCode {
-    return LatestVersionMapper.ensureInitialized()
-        .hashValue(this as LatestVersion);
+    return LatestVersionMapper.ensureInitialized().hashValue(
+      this as LatestVersion,
+    );
   }
 }
 
 extension LatestVersionValueCopy<$R, $Out>
     on ObjectCopyWith<$R, LatestVersion, $Out> {
   LatestVersionCopyWith<$R, LatestVersion, $Out> get $asLatestVersion =>
-      $base.as((v, t, t2) => _LatestVersionCopyWithImpl(v, t, t2));
+      $base.as((v, t, t2) => _LatestVersionCopyWithImpl<$R, $Out>(v, t, t2));
 }
 
 abstract class LatestVersionCopyWith<$R, $In extends LatestVersion, $Out>
@@ -116,19 +133,23 @@ class _LatestVersionCopyWithImpl<$R, $Out>
       $value.packageInfo.copyWith.$chain((v) => call(packageInfo: v));
   @override
   $R call({bool? needUpdate, String? latestVersion, PubPackage? packageInfo}) =>
-      $apply(FieldCopyWithData({
-        if (needUpdate != null) #needUpdate: needUpdate,
-        if (latestVersion != null) #latestVersion: latestVersion,
-        if (packageInfo != null) #packageInfo: packageInfo
-      }));
+      $apply(
+        FieldCopyWithData({
+          if (needUpdate != null) #needUpdate: needUpdate,
+          if (latestVersion != null) #latestVersion: latestVersion,
+          if (packageInfo != null) #packageInfo: packageInfo,
+        }),
+      );
   @override
   LatestVersion $make(CopyWithData data) => LatestVersion(
-      needUpdate: data.get(#needUpdate, or: $value.needUpdate),
-      latestVersion: data.get(#latestVersion, or: $value.latestVersion),
-      packageInfo: data.get(#packageInfo, or: $value.packageInfo));
+    needUpdate: data.get(#needUpdate, or: $value.needUpdate),
+    latestVersion: data.get(#latestVersion, or: $value.latestVersion),
+    packageInfo: data.get(#packageInfo, or: $value.packageInfo),
+  );
 
   @override
   LatestVersionCopyWith<$R2, LatestVersion, $Out2> $chain<$R2, $Out2>(
-          Then<$Out2, $R2> t) =>
-      _LatestVersionCopyWithImpl($value, $cast, t);
+    Then<$Out2, $R2> t,
+  ) => _LatestVersionCopyWithImpl<$R2, $Out2>($value, $cast, t);
 }
+
--- lib/src/models/package_score_model.mapper.dart
+++ lib/src/models/package_score_model.mapper.dart
@@ -1,5 +1,6 @@
 // coverage:ignore-file
 // GENERATED CODE - DO NOT MODIFY BY HAND
+// dart format off
 // ignore_for_file: type=lint
 // ignore_for_file: unused_element, unnecessary_cast, override_on_non_overriding_member
 // ignore_for_file: strict_raw_type, inference_failure_on_untyped_parameter
@@ -21,23 +22,35 @@ class PackageScoreMapper extends ClassMapperBase<PackageScore> {
   final String id = 'PackageScore';
 
   static int? _$grantedPoints(PackageScore v) => v.grantedPoints;
-  static const Field<PackageScore, int> _f$grantedPoints =
-      Field('grantedPoints', _$grantedPoints);
+  static const Field<PackageScore, int> _f$grantedPoints = Field(
+    'grantedPoints',
+    _$grantedPoints,
+  );
   static int? _$maxPoints(PackageScore v) => v.maxPoints;
-  static const Field<PackageScore, int> _f$maxPoints =
-      Field('maxPoints', _$maxPoints);
+  static const Field<PackageScore, int> _f$maxPoints = Field(
+    'maxPoints',
+    _$maxPoints,
+  );
   static int _$likeCount(PackageScore v) => v.likeCount;
-  static const Field<PackageScore, int> _f$likeCount =
-      Field('likeCount', _$likeCount);
+  static const Field<PackageScore, int> _f$likeCount = Field(
+    'likeCount',
+    _$likeCount,
+  );
   static double? _$popularityScore(PackageScore v) => v.popularityScore;
-  static const Field<PackageScore, double> _f$popularityScore =
-      Field('popularityScore', _$popularityScore);
+  static const Field<PackageScore, double> _f$popularityScore = Field(
+    'popularityScore',
+    _$popularityScore,
+  );
   static int? _$downloadCount30Days(PackageScore v) => v.downloadCount30Days;
-  static const Field<PackageScore, int> _f$downloadCount30Days =
-      Field('downloadCount30Days', _$downloadCount30Days);
+  static const Field<PackageScore, int> _f$downloadCount30Days = Field(
+    'downloadCount30Days',
+    _$downloadCount30Days,
+  );
   static List<String> _$tags(PackageScore v) => v.tags;
-  static const Field<PackageScore, List<String>> _f$tags =
-      Field('tags', _$tags);
+  static const Field<PackageScore, List<String>> _f$tags = Field(
+    'tags',
+    _$tags,
+  );
 
   @override
   final MappableFields<PackageScore> fields = const {
@@ -51,12 +64,13 @@ class PackageScoreMapper extends ClassMapperBase<PackageScore> {
 
   static PackageScore _instantiate(DecodingData data) {
     return PackageScore(
-        grantedPoints: data.dec(_f$grantedPoints),
-        maxPoints: data.dec(_f$maxPoints),
-        likeCount: data.dec(_f$likeCount),
-        popularityScore: data.dec(_f$popularityScore),
-        downloadCount30Days: data.dec(_f$downloadCount30Days),
-        tags: data.dec(_f$tags));
+      grantedPoints: data.dec(_f$grantedPoints),
+      maxPoints: data.dec(_f$maxPoints),
+      likeCount: data.dec(_f$likeCount),
+      popularityScore: data.dec(_f$popularityScore),
+      downloadCount30Days: data.dec(_f$downloadCount30Days),
+      tags: data.dec(_f$tags),
+    );
   }
 
   @override
@@ -73,52 +87,63 @@ class PackageScoreMapper extends ClassMapperBase<PackageScore> {
 
 mixin PackageScoreMappable {
   String toJson() {
-    return PackageScoreMapper.ensureInitialized()
-        .encodeJson<PackageScore>(this as PackageScore);
+    return PackageScoreMapper.ensureInitialized().encodeJson<PackageScore>(
+      this as PackageScore,
+    );
   }
 
   Map<String, dynamic> toMap() {
-    return PackageScoreMapper.ensureInitialized()
-        .encodeMap<PackageScore>(this as PackageScore);
+    return PackageScoreMapper.ensureInitialized().encodeMap<PackageScore>(
+      this as PackageScore,
+    );
   }
 
   PackageScoreCopyWith<PackageScore, PackageScore, PackageScore> get copyWith =>
-      _PackageScoreCopyWithImpl(this as PackageScore, $identity, $identity);
+      _PackageScoreCopyWithImpl<PackageScore, PackageScore>(
+        this as PackageScore,
+        $identity,
+        $identity,
+      );
   @override
   String toString() {
-    return PackageScoreMapper.ensureInitialized()
-        .stringifyValue(this as PackageScore);
+    return PackageScoreMapper.ensureInitialized().stringifyValue(
+      this as PackageScore,
+    );
   }
 
   @override
   bool operator ==(Object other) {
-    return PackageScoreMapper.ensureInitialized()
-        .equalsValue(this as PackageScore, other);
+    return PackageScoreMapper.ensureInitialized().equalsValue(
+      this as PackageScore,
+      other,
+    );
   }
 
   @override
   int get hashCode {
-    return PackageScoreMapper.ensureInitialized()
-        .hashValue(this as PackageScore);
+    return PackageScoreMapper.ensureInitialized().hashValue(
+      this as PackageScore,
+    );
   }
 }
 
 extension PackageScoreValueCopy<$R, $Out>
     on ObjectCopyWith<$R, PackageScore, $Out> {
   PackageScoreCopyWith<$R, PackageScore, $Out> get $asPackageScore =>
-      $base.as((v, t, t2) => _PackageScoreCopyWithImpl(v, t, t2));
+      $base.as((v, t, t2) => _PackageScoreCopyWithImpl<$R, $Out>(v, t, t2));
 }
 
 abstract class PackageScoreCopyWith<$R, $In extends PackageScore, $Out>
     implements ClassCopyWith<$R, $In, $Out> {
   ListCopyWith<$R, String, ObjectCopyWith<$R, String, String>> get tags;
-  $R call(
-      {int? grantedPoints,
-      int? maxPoints,
-      int? likeCount,
-      double? popularityScore,
-      int? downloadCount30Days,
-      List<String>? tags});
+  $R call({
+    int? grantedPoints,
+    int? maxPoints,
+    int? likeCount,
+    double? popularityScore,
+    int? downloadCount30Days,
+    List<String>? tags,
+  });
   PackageScoreCopyWith<$R2, $In, $Out2> $chain<$R2, $Out2>(Then<$Out2, $R2> t);
 }
 
@@ -132,37 +157,46 @@ class _PackageScoreCopyWithImpl<$R, $Out>
       PackageScoreMapper.ensureInitialized();
   @override
   ListCopyWith<$R, String, ObjectCopyWith<$R, String, String>> get tags =>
-      ListCopyWith($value.tags, (v, t) => ObjectCopyWith(v, $identity, t),
-          (v) => call(tags: v));
+      ListCopyWith(
+        $value.tags,
+        (v, t) => ObjectCopyWith(v, $identity, t),
+        (v) => call(tags: v),
+      );
   @override
-  $R call(
-          {Object? grantedPoints = $none,
-          Object? maxPoints = $none,
-          int? likeCount,
-          Object? popularityScore = $none,
-          Object? downloadCount30Days = $none,
-          List<String>? tags}) =>
-      $apply(FieldCopyWithData({
-        if (grantedPoints != $none) #grantedPoints: grantedPoints,
-        if (maxPoints != $none) #maxPoints: maxPoints,
-        if (likeCount != null) #likeCount: likeCount,
-        if (popularityScore != $none) #popularityScore: popularityScore,
-        if (downloadCount30Days != $none)
-          #downloadCount30Days: downloadCount30Days,
-        if (tags != null) #tags: tags
-      }));
+  $R call({
+    Object? grantedPoints = $none,
+    Object? maxPoints = $none,
+    int? likeCount,
+    Object? popularityScore = $none,
+    Object? downloadCount30Days = $none,
+    List<String>? tags,
+  }) => $apply(
+    FieldCopyWithData({
+      if (grantedPoints != $none) #grantedPoints: grantedPoints,
+      if (maxPoints != $none) #maxPoints: maxPoints,
+      if (likeCount != null) #likeCount: likeCount,
+      if (popularityScore != $none) #popularityScore: popularityScore,
+      if (downloadCount30Days != $none)
+        #downloadCount30Days: downloadCount30Days,
+      if (tags != null) #tags: tags,
+    }),
+  );
   @override
   PackageScore $make(CopyWithData data) => PackageScore(
-      grantedPoints: data.get(#grantedPoints, or: $value.grantedPoints),
-      maxPoints: data.get(#maxPoints, or: $value.maxPoints),
-      likeCount: data.get(#likeCount, or: $value.likeCount),
-      popularityScore: data.get(#popularityScore, or: $value.popularityScore),
-      downloadCount30Days:
-          data.get(#downloadCount30Days, or: $value.downloadCount30Days),
-      tags: data.get(#tags, or: $value.tags));
+    grantedPoints: data.get(#grantedPoints, or: $value.grantedPoints),
+    maxPoints: data.get(#maxPoints, or: $value.maxPoints),
+    likeCount: data.get(#likeCount, or: $value.likeCount),
+    popularityScore: data.get(#popularityScore, or: $value.popularityScore),
+    downloadCount30Days: data.get(
+      #downloadCount30Days,
+      or: $value.downloadCount30Days,
+    ),
+    tags: data.get(#tags, or: $value.tags),
+  );
 
   @override
   PackageScoreCopyWith<$R2, PackageScore, $Out2> $chain<$R2, $Out2>(
-          Then<$Out2, $R2> t) =>
-      _PackageScoreCopyWithImpl($value, $cast, t);
+    Then<$Out2, $R2> t,
+  ) => _PackageScoreCopyWithImpl<$R2, $Out2>($value, $cast, t);
 }
+
